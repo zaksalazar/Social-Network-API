52ed32abcc066e3d22a94c76e3573c1fe688911a
zaksalazar <zsalazar.tv@gmail.com>
2023-02-14 21:10:13 -0800

    completed and tested all routes

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -85,13 +85,13 @@ router.post("/:thoughtId/reactions", (req, res) => {
 },
 )
 // //TODO: ROUTE TO DELETE A REACTION ON A THOUGHT
-router.delete("/:thoughtId/reactions/:reactionId", (req, res) => {
-  Thought.findOneAndUpdate(
-    { _id: req.params.thoughtId },
-    { $pull: { reactions: { reactionId: req.params.reactionId } } }
-  )
-    .then((result) => res.status(200).json(result))
-    .catch((err) => res.status(500).json(err));
-  });
+  router.delete("/:thoughtId/reactions/:reactionId", (req, res) => {
+    Thought.findOneAndUpdate(
+      { _id: req.params.thoughtId },
+      { $pull: { reactions: { reactionId: req.params.reactionId } } }
+    )
+      .then((result) => res.status(200).json(result))
+      .catch((err) => res.status(500).json(err));
+    });
 
 module.exports = router;
